1153527c20f4218b180a756ae544a13571f41edb
flipab <felip.ab@gmail.com>
2021-10-07 10:46:47 +0000

    lista de numeros  no ListaNumerosComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,18 @@
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
+
+@Component({
+  selector: 'app-lista-numeros',
+  template: `
+  `,
+  styles: [`
+  `],
+})
+export class ListaNumerosComponentMock {
+}
 
 describe('AppComponent', () => {
 
@@ -12,14 +23,13 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent,
+        ListaNumerosComponentMock,
       ],
     }).compileComponents();
   });
-
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
 });
--- src/app/components/lista-numeros/lista-numeros.component.ts
+++ src/app/components/lista-numeros/lista-numeros.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Numeros } from 'src/app/models/numeros';
 
 @Component({
   selector: 'app-lista-numeros',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-numeros.component.css'],
 })
 export class ListaNumerosComponent implements OnInit {
+
+  public numeros: Numeros[] = [
+    new Numero(
+
+      ),
 
   constructor() { }
 
