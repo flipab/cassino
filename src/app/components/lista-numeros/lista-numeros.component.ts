ac07fd8366ab142cc67e1f0adc946660c6775cb2
flipab <felip.ab@gmail.com>
2021-10-07 10:46:33 +0000

    lista de numeros  no ListaNumerosComponent

--- src/app/components/lista-numeros/lista-numeros.component.ts
+++ src/app/components/lista-numeros/lista-numeros.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-lista-numeros',
   templateUrl: './lista-numeros.component.html',
-  styleUrls: ['./lista-numeros.component.css']
+  styleUrls: ['./lista-numeros.component.css'],
 })
 export class ListaNumerosComponent implements OnInit {
 
