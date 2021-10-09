84ece88a8c6be32109e3bb9af8fd122b4d9588e6
flipab <felip.ab@gmail.com>
2021-10-09 22:25:50 +0000

    Constructor implementation is missing.ts(2390)

--- src/app/components/lista-naipes/lista-naipes.component.ts
+++ src/app/components/lista-naipes/lista-naipes.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaNaipesComponent implements OnInit {
 
+  public naipes: Naipe[] = [
+    new Naipe(1, 'paus'),
+    new Naipe(2, 'ouros'),
+    new Naipe(3, 'copas'),
+    new Naipe(4, 'espadas'),
+  ]
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  public selecionaNaipe(naipe: Naipe) {
+    this.naipeSelecionado.emit(naipe);
   }
 
 }
